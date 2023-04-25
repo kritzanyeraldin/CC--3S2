e43b4a722f878a119d93b1d2943416a69ce1082c
Jaime Acuña <jaime.acuna.n@uni.pe>
2023-04-25 14:16:31 -0500

    sprint1
    Colocar S/O jugador rojo

---
+++ Proyecto-3S2/SOS/sprint 1/Board.py
@@ -0,0 +1,14 @@
+class Board:
+    def __init__(self, board_size):
+        self.board_size = board_size
+        self.board = self.create_board()
+
+    def get_board_size(self):
+        return self.board_size
+
+    def create_board(self):
+        self.board = [[None for i in range(self.board_size)] for j in range(self.board_size)]
+        return self.board
+
+
+
