c6f3afdde7b5d00cabbf6fc0c24d653c13791b01
Jaime Acuña <jaime.acuna.n@uni.pe>
2023-04-26 01:50:48 -0500

    completar SOS

--- Proyecto-3S2/SOS/sprint 1/Board.py
+++ Proyecto-3S2/SOS/sprint 1/Board.py
@@ -11,7 +11,7 @@ class Board:
         return self.board
 
     def get_piece(self, row, col):
-        if (0<=row<self.board_size) and (0<=col<self.board_size):
+        if (0 <= row < self.board_size) and (0 <= col < self.board_size):
             return self.board[row][col]
         else:
             return None
@@ -34,11 +34,83 @@ class Board:
         if piece not in valid_pieces:
             return 'Pieza no valida'
 
-        if not (self.get_piece(row,col) == None):
+        if not (self.get_piece(row, col) == None):
             return 'Casilla ocupada'
 
         # Asignación de la pieza al tablero
         self.board[row][col] = piece
 
+    def rows(self):
+        row = 0
+        m = ''
+        while row < self.get_board_size():
+            for col in range(self.get_board_size()):
+                m += self.get_piece(row, col)
+            if m == 'SOS':
+                return True
+                break
+            else:
+                m=''
+                row += 1
+                continue
+        return False
+
+    def cols(self):
+        col=0
+        m=''
+        while col<self.get_board_size():
+            for row in range(self.get_board_size()):
+                m+=self.get_piece(row,col)
+            if m=='SOS':
+                return True
+                break
+            else:
+                m=''
+                col+=1
+                continue
+        return False
+
+    def check_diagonals(self):
+        size = self.get_board_size()
+        for i in range(size):
+            for j in range(size):
+                for k in range(4, min(size - i, size - j) + 1):
+                    # diagonal desde la esquina superior izquierda hasta la inferior derecha
+                    m = ""
+                    for x in range(k):
+                        m += self.get_piece(i+x,j+x)
+                    if m == "SOS":
+                        return True
+                    # diagonal desde la esquina inferior izquierda hasta la superior derecha
+                    if i >= k - 1:
+                        n = ""
+                        for x in range(k):
+                            n += self.get_piece(i-x,j+x)
+                        if n == "SOS":
+                            return True
+        return False
 
 
+    def complete_SOS(self):
+
+            if m=='SOS':
+                print(f'complete')
+
+
+
+board = Board(4)
+print(board.get_board())
+
+
+for m in range(4):
+    i = int(input('Ingrese fila: '))
+    j = int(input('Ingrese columna: '))
+    p = input('Ingrese una pieza: ')
+    board.insert_piece(i, j, p)
+
+print(board.get_board())
+if board.rows():
+    print('win')
+
+else:
+    print('lose')
